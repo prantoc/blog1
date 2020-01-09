2115481cf4c84ab2d5daff5fcaec96d757ddbdb1
Pranto <chakrabortypranto39@gmail.com>
2020-01-09 22:04:16 +0600

    change slider design in work-img-slider page

--- resources/views/frontend/work-img-slider.blade.php
+++ resources/views/frontend/work-img-slider.blade.php
@@ -64,36 +64,34 @@
  --}}
 
 
-
-@extends('layouts.frontend.home-design')
+@extends('layouts.frontend.work-design')
 @section('content')
-<div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel" data-interval="false">
-  <ol class="carousel-indicators">
-    <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
-    <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
-    <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
-  </ol>
-  <div class="carousel-inner">
-     @foreach($cats as $key => $c)
-    <div class="carousel-item {{$key == 0 ? 'active' : '' }} active">
-    @if($c->file_type == 1)
-      <div class="carousel-caption text-dark" style="font-size: 22px; font-weight: bold;position: absolute;top: 0%;">
-        
-        <p>{!! $c->details !!}</p>
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel" data-interval="false">
+   <ol class="carousel-indicators">
+  @foreach($cats as $key => $c)
+      <li data-target="#carouselExampleIndicators" data-slide-to="{{ $c->id }}" class="{{$key == 0 ? 'active' : '' }}"></li>
+     {{--  <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="5"></li> --}}
+   @endforeach
+   </ol>
+   <div class="carousel-inner" role="listbox">
+      <!-- Slide One - Set the background image for this slide in the line below -->
+      @foreach($cats as $key => $c)
+      <div class="carousel-item {{$key == 0 ? 'active' : '' }}" style="background-image: url('{{asset('workfile/img/'.$c->file)}}')">
       </div>
-       @elseif($c->file_type == 2)
-      <img src="{{asset('assets/img/t.jpg')}}" class="d-block w-100" alt="">
-          @endif
-    </div>
-    @endforeach
-  </div>
-  <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
+      @endforeach
+   </div>
+
+<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+<span class="sr-only">Previous</span>
+</a>
+<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+<span class="carousel-control-next-icon" aria-hidden="true"></span>
+<span class="sr-only">Next</span>
+</a>
 </div>
 @endsection
